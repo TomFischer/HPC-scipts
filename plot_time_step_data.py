c9569539a721ff44a95e998e2df6c4257dcc9c1d
Thomas Fischer <thomas.fischer@ufz.de>
2018-01-16 14:59:34 +0100

    Cleanup plot_time_step_data.

--- plot_time_step_data.py
+++ plot_time_step_data.py
@@ -3,23 +3,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from numpy import genfromtxt
-import pandas as pd
 import sys
 import pylab
-import csv
 
-# reading and parsing functions
 
-#def readData(filename):
-#    time_step_number = []
-#    iterations = []
-#    assembly_time = []
-#    solver_time = []
-#
-#    for line in open(filename):
-#        temperature.append(line)
-
-data = genfromtxt(sys.argv[1], delimiter=' ')
 
 nonlinear_iterations = data[:,1]
 assembly_time = data[:,2]

7b4f04f6b54f9621204c95d4c3470ce5a674e2c4
Thomas Fischer <thomas.fischer@ufz.de>
2018-08-01 06:15:30 +0200

    correct project file name, use the variable for the project file name

--- createHTCubeExamles.sh
+++ createHTCubeExamles.sh
@@ -13,10 +13,10 @@ for solver in "${PETSc_KSPSolvers[@]}"
 do
     for pc in "${PETSc_Preconditioners[@]}"
     do
-        project_file=Ra_795_${solver}_${pc}_rtol_1e-10_atol_1e-10_tend_${tend}.prj
+        project_file=Ra_795_fault_${solver}_${pc}_rtol_1e-10_atol_1e-10_tend_${tend}.prj
         cp templates/${model_revision}.prj ${project_dir}/${project_file}
-        sed --in-place "s/LINEARSOLVER/${solver}/" ${project_dir}/Ra_795_${solver}_${pc}_rtol_1e-10_atol_1e-10_tend_${tend}.prj
-        sed --in-place "s/PC/${pc}/" ${project_dir}/Ra_795_${solver}_${pc}_rtol_1e-10_atol_1e-10_tend_${tend}.prj
+        sed --in-place "s/LINEARSOLVER/${solver}/" ${project_dir}/${project_file}
+        sed --in-place "s/PC/${pc}${PETSc_Preconditioner_Options[$pc_options_idx]}/" ${project_dir}/${project_file}
         submit_script=${solver}_${pc}_1e-10_dirichlet.sh
         cp templates/jureca_ht_script ${project_dir}/${submit_script}
         sed --in-place "s/PROJECTFILE/${project_file}/" ${project_dir}/${submit_script}

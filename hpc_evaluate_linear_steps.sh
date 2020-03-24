384a38ed42e3ce19549857d82f362c458532826e
Thomas Fischer <thomas.fischer@ufz.de>
2020-03-24 14:37:22 +0100

    [hpc_evaluate_linear_steps.sh] script to evaluate the linear steps.

---
+++ hpc_evaluate_linear_steps.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# partition the out.txt file
+mkdir part_out_files
+for i in `seq 0 $2`;
+do
+    if [ ! -f part_out_files/$i.txt ];
+    then
+        grep "\[$i\]" $1 > part_out_files/$i.txt
+    fi
+done
+
+# process the files in part_out_files folder
+mkdir processed_data
+for i in `seq 0 $2`;
+do
+    python ~/w/HPC-scripts/process_ht_experiments.py --linearsteps_as_csv part_out_files/$i.txt > processed_data/$i.txt
+done
+
+

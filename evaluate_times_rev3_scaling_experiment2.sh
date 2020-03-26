e1ddc88cfae4efa574a9788d73fff72abc22ca20
Thomas Fischer <thomas.fischer@ufz.de>
2020-03-26 18:11:22 +0100

    Generalize the binary path.

--- evaluate_times_rev3_scaling_experiment2.sh
+++ evaluate_times_rev3_scaling_experiment2.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-io_0=`~/w/HPC-scripts/HPC-scipts/evaluate_io_time_ts_0.sh $1`
-io_50=`~/w/HPC-scripts/HPC-scipts/evaluate_io_time_ts_50.sh $1`
-execution=`~/w/HPC-scripts/HPC-scipts/evaluate_execution_time.sh $1`
+bin_path=$PROJECT_ogs6hpc
+
+io_0=`${bin_path}/w/HPC-scripts/evaluate_io_time_ts_0.sh $1`
+io_50=`${bin_path}/w/HPC-scripts/evaluate_io_time_ts_50.sh $1`
+execution=`${bin_path}/w/HPC-scripts/evaluate_execution_time.sh $1`
 echo ${io_0} ${io_50} ${execution}

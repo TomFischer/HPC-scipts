4d2cc6fbad7fd1d383085678692336325941138d
Thomas Fischer <thomas.fischer@ufz.de>
2020-03-27 06:02:58 +0100

    Introduce full path to script/templates.

--- createHTCubeScalingSubmitScriptForJUWELS.sh
+++ createHTCubeScalingSubmitScriptForJUWELS.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-declare -a compute_nodes=("9" "10") # "16" "32" "50" "64" "90" "96" "128" "160" "180" "192" "256")
+declare -a compute_nodes=("9" "10" "16" "32" "50" "64" "90" "96" "128" "160" "180" "192" "256")
 declare -a PETSc_KSPSolvers=("gmres")
 declare -a PETSc_Preconditioners=("jacobi")
 declare -a PETSc_Preconditioner_Options=("" " -pc_hypre_type boomeramg" "")
@@ -14,6 +14,7 @@ branch_name="ConvergenceImprovements"
 runtime=$1
 base_data_path=$2
 base_output_path=$3
+script_home=$2/w/HPC-scripts/
 
 for nodes in "${compute_nodes[@]}"
 do
@@ -31,12 +32,12 @@ do
         for pc in "${PETSc_Preconditioners[@]}"
         do
             project_file=Ra_795_fault_${solver}_${pc}_${branch_name}_scaling_experiment_50_ts.prj
-            cp templates/FaultedCube_HT_${branch_name}_scaling_experiment_50_ts.prj ${project_dir}/${project_file}
+            cp $script_home/templates/FaultedCube_HT_${branch_name}_scaling_experiment_50_ts.prj ${project_dir}/${project_file}
             sed --in-place "s/LINEARSOLVER/${solver}/" ${project_dir}/${project_file}
             sed --in-place "s/PC/${pc}${PETSc_Preconditioner_Options[$pc_options_idx]}/" ${project_dir}/${project_file}
 
             submit_script=${solver}_${pc}_HT_FracturedCube_Scaling.sh
-            cp templates/juwels_ht_script ${project_dir}/${submit_script}
+            cp $script_home/templates/juwels_ht_script ${project_dir}/${submit_script}
             sed --in-place "s/PROJECTFILE/${project_file}/" ${project_dir}/${submit_script}
             sed --in-place "s/NTASKS/${ntasks}/" ${project_dir}/${submit_script}
             sed --in-place "s/NNODES/${nodes}/" ${project_dir}/${submit_script}

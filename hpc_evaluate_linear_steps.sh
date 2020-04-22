048b07b1b35df8b0c1fcdc32a9955d51e2d13c1c
Thomas Fischer <thomas.fischer@ufz.de>
2020-04-22 14:34:54 +0200

    [hpc_evaluate_linear_steps.sh] Solve last index issues.

--- hpc_evaluate_linear_steps.sh
+++ hpc_evaluate_linear_steps.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
+upper=$((${2}-1))
+
 # partition the out.txt file
 mkdir part_out_files
-for i in `seq 0 $2`;
+for i in `seq 0 $upper`;
 do
     if [ ! -f part_out_files/$i.txt ];
     then
@@ -15,13 +17,13 @@ wd=`pwd`
 detailed_output_folder=processed_data/linearsteps
 script_path=$HOME/w/HPC-scripts/
 mkdir -p $detailed_output_folder
-for i in `seq 0 $2`;
+for i in `seq 0 $upper`;
 do
     python ${script_path}/process_ht_experiments.py --linearsteps_as_csv part_out_files/$i.txt > $detailed_output_folder/$i.txt
 done
 
 cd $detailed_output_folder
-python ${script_path}/combine_detailed_data.py 0 $2
+python ${script_path}/combine_detailed_data.py 0 $upper
 cd $wd
 
 

62e0e296cfa5124c6f24f8dfcc736f73c2277a8b
Thomas Fischer <thomas.fischer@ufz.de>
2020-05-06 11:16:05 +0200

    Update makeOGS script to new project and new libraries.

--- JUWELS/makeOGS.sh
+++ JUWELS/makeOGS.sh
@@ -27,7 +27,9 @@ cd ..
 mkdir br
 cd br
 
-cmake ../s/ -DCMAKE_BUILD_TYPE=Release -DOGS_EIGEN_DYNAMIC_SHAPE_MATRICES=Off -DOGS_USE_PCH=Off -DVTK_DIR=$PROJECT_chlz25/hlz250/$base_working_dir/vtk/vtk-8/build_release/ -DEIGEN3_INCLUDE_DIR=/gpfs/software/juwels/stages/2018b/software/Eigen/3.3.5-GCCcore-8.2.0/include -DOGS_BUILD_METIS=On -DOGS_BUILD_UTILS=On -DOGS_USE_CONAN=OFF
+PROJECT=$PROJECT_ogs6hpc2
+
+cmake ../s/ -DCMAKE_BUILD_TYPE=Release -DOGS_EIGEN_DYNAMIC_SHAPE_MATRICES=Off -DOGS_USE_PCH=Off -DVTK_DIR=$PROJECT/$base_working_dir/vtk/vtk-8/build_release/ -DEIGEN3_INCLUDE_DIR=/gpfs/software/juwels/stages/2019a/software/Eigen/3.3.7-GCCcore-8.3.0/include -DOGS_BUILD_METIS=On -DOGS_BUILD_UTILS=On -DOGS_USE_CONAN=OFF
 
 make -j20 partmesh
 
@@ -35,5 +37,5 @@ make -j20 partmesh
 cd ..
 mkdir br_petsc
 cd br_petsc
-CC=`which mpicc` CXX=`which mpic++` cmake ../s/ -DCMAKE_BUILD_TYPE=Release -DOGS_EIGEN_DYNAMIC_SHAPE_MATRICES=Off -DOGS_USE_PCH=Off -DVTK_DIR=$PROJECT_chlz25/hlz250/$base_working_dir/vtk/vtk-8/build_release/ -DEIGEN3_INCLUDE_DIR=/gpfs/software/juwels/stages/2018b/software/Eigen/3.3.5-GCCcore-8.2.0/include -DOGS_USE_PETSC=On -DOGS_USE_MPI=On -DOGS_USE_CONAN=OFF
+CC=`which mpicc` CXX=`which mpic++` cmake ../s/ -DCMAKE_BUILD_TYPE=Release -DOGS_EIGEN_DYNAMIC_SHAPE_MATRICES=Off -DOGS_USE_PCH=Off -DVTK_DIR=$PROJECT/$base_working_dir/vtk/vtk-8/build_release/ -DEIGEN3_INCLUDE_DIR=/gpfs/software/juwels/stages/2019a/software/Eigen/3.3.7-GCCcore-8.3.0/include -DOGS_USE_PETSC=On -DOGS_USE_MPI=On -DOGS_USE_CONAN=OFF
 make -j20 ogs
